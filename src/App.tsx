01375eda197517f9f34977f3b56a2ce8ff46873a
Santhoshi Vijaykumar Nanikeri <santhoshi.nanikeri@gmail.com>
2025-03-20 21:33:34 +0530

    Sample commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ import SubcategoryTypePage from "./pages/private/Library/SubcategoryTypePage";
 import { SnackbarProvider } from "./Components/SnackbarContext";
 const queryClient = new QueryClient();
 
+// Hi
+
 const App = () => {
   return (
     <SnackbarProvider>
